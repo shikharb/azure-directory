e456a301e440678704afe83950468ff44dd31388
Shikhar Bhagat <shikhar.bhagat.1@outlook.com>
2020-10-25 17:44:55 +1300

    Updated script to add Powershell command to remove existing (deprecated) AzureRm module

--- InstallPowershellAzureModule.ps1
+++ InstallPowershellAzureModule.ps1
@@ -2,6 +2,8 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
       'Az modules installed at the same time is not supported. ' +
       'Recommend uninstalling Azure Powershell from the computer before installing new Az Modules.')
+    Write-Host  ('Run the following command before re-running this script:')
+    Write-Host ('Uninstall-AzureRm -Confirm') -ForegroundColor Cyan
 } else {
     Install-Module -Name Az -AllowClobber -Scope CurrentUser
 }
